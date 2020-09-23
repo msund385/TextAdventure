fe9f0a7fb990f5f3720a309638719240b74f1c28
msund385 <ms43004foru@gmail.com>
2020-09-23 11:03:31 -0400

    initial commit

---
+++ textAdventure/textAdventure/Flow1.cs
@@ -0,0 +1,155 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace textAdventure
+{
+    using System;
+
+  
+    internal class Flow1
+    {
+       
+       //Scene1--Scene3-Scene5-Scene7
+       //Scene1--Scene2--Scene4--Scene6
+        public static void Scene1()
+        {
+            Console.WriteLine("So you got away from the wild boars,\n you stop to relax and check your wounds");
+            Console.WriteLine("You see bandages laying in the distance or Gold in the other direction : 1. Bandage 2. Gold.");
+            string gold = Console.ReadLine();
+            var w = 0;
+            Int32.TryParse(gold, out w);
+            if (w == 1)
+            {
+               Scene2();
+            }
+            else if (w == 2)
+            {
+                Scene3();
+            }
+            while(true)
+            {
+               
+            }
+            
+        }
+          
+        public static void Scene2()
+        {
+            Console.WriteLine("You have survived thus far, you will be rewarded!!!");
+            Console.WriteLine("What is your dog's name?");
+            string duck  = Console.ReadLine();
+            Scene4();
+        }
+        
+
+        
+            
+           
+            
+           
+            
+                
+            
+            
+            
+
+        public static void Scene3()
+        {
+            Console.WriteLine("You want those riches,get that Money, here's some advice from Warren");
+            Console.WriteLine("Only buy something that you’d be perfectly happy to hold \nif the market shuts down for ten years.”");
+            Console.WriteLine("Any thing in the world,what is it...");
+            string prize = Console.ReadLine();
+            var d = 1;
+            Scene5();
+
+           
+            
+                
+            
+
+          
+            
+
+            
+        }
+        public static void Scene4()
+        {   Console.WriteLine($"Your dog's name is (Scene2.duck)");
+            Console.WriteLine("Your dog leads you to a make shift shack in the distance ");
+            Scene6();
+
+
+        }
+
+        public static void Scene5()
+        {
+            Console.WriteLine ("Your (prize) is weighing you down, you stumble then take a seat next to an old oak tree");
+            Console.WriteLine("You are feeling weak, you cant eat (prize) , you pass out");
+            Scene7();
+
+        }
+
+        public static void Scene6()
+        {
+            Console.WriteLine("You walk up , pull on the door, it opens....");
+            Console.WriteLine("You and (duck) walk in, sitting in the chair is (Program.friend)");
+            Console.WriteLine("(Program.friend) lead you to safety, you win a cruise and vacation from the ruff life");
+            System.Environment.Exit(0);
+        }
+
+         public static void Scene7()
+        {
+            Console.WriteLine("You awaken from a tap on the shoulder, it's your buddy(Program.friend)");
+            Console.WriteLine("They give you nourishment and help you carry the gold and (prize)");
+            Console.WriteLine("You recover and with your share of Gold and (prize) you bought a BW3 franchise");
+            System.Environment.Exit(0);
+        }
+
+
+
+
+    }
+}
+       
+
+    
+    
+    
+
+
+
+
+
+
+
+    
+   
+    
+    
+
+    
+    
+    
+    
+    
+
+   
+    
+    
+ 
+
+   
+
+    
+ 
+
+
+    
+
+    
+    
+    
+    
+    
+    
+
---
+++ textAdventure/textAdventure/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Data;
+using System.Diagnostics;
+
+
+
+
+namespace textAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, welcome to your text adventure,choose your fate");
+            Console.WriteLine("What is the name of our courageous hero?");
+            string hero = Console.ReadLine(); //stores user input
+            Console.WriteLine("What is the name of your brave friend");
+            string friend = Console.ReadLine(); //stores user input
+            Console.WriteLine("What is your dazzling magic?");
+            string magic = Console.ReadLine(); //stores user input
+            Console.WriteLine($"Welcome to the land of Cheyenia, where {hero} lives,breathes and");
+            Console.WriteLine($"survives with his pal {friend}.You must use your power of {magic}");
+            Console.WriteLine("to save the citizens from destruction. May the swartz be with you!!!");
+            
+           
+            
+            var cursorTop = Console.CursorTop;// changes background and text color
+            var colorOne = Console.ForegroundColor;
+            var colorTwo = Console.BackgroundColor;
+            var stopwatch = Stopwatch.StartNew();
+            var time = TimeSpan.Zero;
+           
+           
+            while (!Console.KeyAvailable)// changes console output background and text
+            {
+                var currentValue = stopwatch.Elapsed;
+                if(currentValue - time < TimeSpan.Zero) continue;
+                time  = currentValue;
+                Console.ForegroundColor = Console.ForegroundColor == colorOne
+                    ? colorTwo
+                    : colorOne;
+                    
+                Console.BackgroundColor = Console.BackgroundColor == colorOne
+                    ? colorTwo
+                    : colorOne;
+                Console.SetCursorPosition(0, cursorTop);
+                Console.WriteLine("Do you go right or left,\n enter 1 for right or 2 for left!!!!");
+                var pick = Console.ReadLine();//assigns user input to a variable
+                
+                int x = 0;
+                Int32.TryParse(pick, out x);//changes string to int
+               
+                if (x == 1)//takes answer 1 or 2 and supplies corresponding answer
+                {
+                    Console.WriteLine("Bad Choice!!You find yourself in the woods, \n you ran into a pack of wild boars!! You got bitten");
+                    Flow1.Scene1();
+                }
+                else if (x == 2)
+                {
+                    Console.WriteLine("Good choice!! You hit a clearing and found a water source.\n You get some relaxation time and ponder your next move");
+                    Flow1.Scene2();
+                }
+               
+
+            }
+
+
+        }
+
+
+    }
+
+}    
+
+
+
+
+
+
+
+
+                
+                
+                
+
+        
+       
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+            
+    
+           
+
+                  
+
+
+
+                
+    
+            
+               
+                
+
+                
+                    
+
+
+
+
+            
+            
+
+        
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+
+   
+
+        
+            
+            
+            
+            
+        
+       
+
+        
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+  
+
+
+
+   
+                  
+            
+
+
+
+
+
+
+  
+        
+        
+        
+
+
+       
+  
+
+
+
+
+        
+            
+            
+            
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+    
+
+    
+    
+
