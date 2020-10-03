978ff6614d5da6b39e5c6c3625cd5c5392c3288a
msund385 <ms43004foru@gmail.com>
2020-10-02 23:49:59 -0400

    Added music,color schemes

---
+++ textAdventure/textAdventure/Music.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.Wave;
+using System;
+using System.Runtime.Serialization;
+using System.Threading;
+using WMPLib;
+using System.Media;
+
+namespace TextAdventure
+{
+    public class Music//class Music
+    {
+        public static void Mus()//method Mus
+        {
+
+            SoundPlayer player = new SoundPlayer();//creates SoundPlayer
+            player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "0740.wav";//puts location of .wav file in variable player
+            player.Play();//plays variable player
+
+
+            
+                
+
+
+
+        }
+      
+
+            
+        
+
+    }
+
+}
+        
+            
+
+
+           
+
+
+
